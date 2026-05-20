fd93b79667b97725124c7feae0bb2e1b7e2ee957
Mike Jarvis <michael@jarvis.net>
2026-05-20 12:46:30 -0400

    Update to version 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
