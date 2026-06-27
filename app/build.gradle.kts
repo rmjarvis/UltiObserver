c9468d5525cbe0b42d0c8bd08e6ede841f36e8af
Mike Jarvis <michael@jarvis.net>
2026-06-27 13:43:46 -0400

    Bump version to 1.1.0alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "rmjarvis.ultiobserver"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.1.0alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
