700f4ec5223df97d4831e16f81d65b7634287fd4
Mike Jarvis <michael@jarvis.net>
2026-07-07 10:26:53 -0400

    Bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 3
-        versionName = "1.1.0alpha"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
