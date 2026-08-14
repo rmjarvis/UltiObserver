a151ccb0e3183cfa8cf69614b9b3ddbce6e8e7bf
Mike Jarvis <michael@jarvis.net>
2026-08-14 10:25:45 -0400

    Version 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 5
-        versionName = "1.3.0alpha"
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
